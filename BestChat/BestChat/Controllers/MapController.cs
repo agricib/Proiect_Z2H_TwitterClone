32f4233c6fb4c88893100b9cedfbc2aad6553ae4
agricib <agricibogdan@gmail.com>
2014-11-11 15:46:14 +0200

    commit multiple changes

---
+++ BestChat/BestChat/Controllers/MapController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapExample.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SimpleMap()
+        {
+            return View();
+        }
+        public ActionResult HeatMap()
+        {
+            return View();
+        }
+        public ActionResult SignalR()
+        {
+            return View();
+        }
+    }
+}
