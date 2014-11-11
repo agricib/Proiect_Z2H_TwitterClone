0102f4ec376a9aeeb71eb73e5c06e4ca97cb20e8
agricib <agricibogdan@gmail.com>
2014-11-11 18:22:32 +0200

    refUserMigration

--- BestChat/DataModels/Migrations/201411111621218_InitialCreate.cs
+++ BestChat/DataModels/Migrations/201411111621218_InitialCreate.cs
@@ -3,7 +3,7 @@ namespace DataModels.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialMigration : DbMigration
+    public partial class InitialCreate : DbMigration
     {
         public override void Up()
         {
--- BestChat/DataModels/Migrations/Configuration.cs
+++ BestChat/DataModels/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace DataModels.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DataModels.UniversityContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataModels.UserContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(DataModels.UniversityContext context)
+        protected override void Seed(DataModels.UserContext context)
         {
             //  This method will be called after migrating to the latest version.
 
