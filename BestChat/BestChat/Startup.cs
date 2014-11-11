87efd016165cdc7e779242b8b9c9fd384f19fd8c
agricib <agricibogdan@gmail.com>
2014-11-11 13:59:57 +0200

    Adding WebFiles

---
+++ BestChat/BestChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BestChat.Startup))]
+namespace BestChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
