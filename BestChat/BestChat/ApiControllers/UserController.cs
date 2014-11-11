a76bbfa95d2b188275499969fab833ee11abdc62
agricib <agricibogdan@gmail.com>
2014-11-11 18:54:12 +0200

    add UserController

---
+++ BestChat/BestChat/ApiControllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using User.Entities;
+
+namespace BestChat.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IEnumerable<UserInfo> Get()
+        {
+           using( var context = new UserContext()){
+               return context.UserSet;
+
+            }
+        }
+    }
+}
