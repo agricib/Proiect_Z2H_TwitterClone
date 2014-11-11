58f0c773c92a1d7ed03d2c6d1d113c8711ee0c7d
agricib <agricibogdan@gmail.com>
2014-11-11 17:55:20 +0200

    AddingEntities

---
+++ BestChat/User.Entities/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Entities.BaseEntity;
+
+namespace User.Entities
+{
+      public class UserInfo : BaseEntity<int>
+        {
+            public string UserName { get; set; }
+
+            public string Name { get; set; }
+
+            public double Latitude { get; set; }
+
+            public double Longitude { get; set; }
+
+            public DateTime PositionDate { get; set; }
+
+            public bool Online { get; set; }
+ 
+        }
+    
+}
