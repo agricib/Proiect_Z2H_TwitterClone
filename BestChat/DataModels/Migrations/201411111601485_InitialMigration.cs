f5df2c43ac4194402c3fdf4c3674f332a61bcb3b
agricib <agricibogdan@gmail.com>
2014-11-11 18:03:13 +0200

    UpdateEntities

---
+++ BestChat/DataModels/Migrations/201411111601485_InitialMigration.cs
@@ -0,0 +1,31 @@
+namespace DataModels.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserInfoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Name = c.String(),
+                        Latitude = c.Double(nullable: false),
+                        Longitude = c.Double(nullable: false),
+                        PositionDate = c.DateTime(nullable: false),
+                        Online = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UserInfoes");
+        }
+    }
+}
