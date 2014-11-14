c44ee1222bee9ea010a32796e9bdb1aa17e32553
andreeabr <andreea.g.bruma@gmail.com>
2014-11-14 11:28:37 +0200

    OptimizareCod

--- BestChat/BestChat/Chat.cs
+++ BestChat/BestChat/Chat.cs
@@ -13,8 +13,8 @@ namespace BestChat
     [Authorize]
     public class Chat : Hub
     {
-        private readonly static ConnectionMapping<string> _connections = new ConnectionMapping<string>();
-       
+        
+
         public void RegisterMe()
         {
             var name = Context.User.Identity.Name;
@@ -37,45 +37,6 @@ namespace BestChat
                 db.SaveChanges();
             }
 
-        }
-        //public void SendChatMessage(string who, string message)
-        //{
-        //    string name = Context.User.Identity.Name;
-
-        //    foreach (var connectionId in _connections.GetConnections(who))
-        //    {
-        //        Clients.Client(connectionId).addChatMessage(name + ": " + message);
-        //    }
-        //}
-
-        public override Task OnConnected()
-        {
-            string name = Context.User.Identity.Name;
-
-            _connections.Add(name, Context.ConnectionId);
-
-            return base.OnConnected();
-        }
-
-        //public override Task OnDisconnected(bool stopCalled)
-        //{
-        //    string name = Context.User.Identity.Name;
-
-        //    _connections.Remove(name, Context.ConnectionId);
-
-        //    return base.OnDisconnected(stopCalled);
-        //}
-
-        public override Task OnReconnected()
-        {
-            string name = Context.User.Identity.Name;
-
-            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
-            {
-                _connections.Add(name, Context.ConnectionId);
-            }
-
-            return base.OnReconnected();
         }
 
     }
--- BestChat/BestChat/ChatHub.cs
+++ BestChat/BestChat/ChatHub.cs
@@ -10,14 +10,7 @@ namespace BestChat
         public void Send(string names, string message)
         {
             string name = Context.User.Identity.Name;
-            //Clients.All.addNewMessageToPage(name, message);
             Clients.Group(names).addNewMessageToPage(name + ": " + message);
-        }
-        public void SendChatMessage(string who, string message)
-        {
-            string name = Context.User.Identity.Name;
-
-            Clients.Group(who).addChatMessage(name + ": " + message);
         }
 
         public override Task OnConnected()
