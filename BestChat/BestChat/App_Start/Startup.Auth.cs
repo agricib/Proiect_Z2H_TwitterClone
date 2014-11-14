4395650fffae08305621eb1520963c79c0eca332
OndinaR <ondina.rosoiu@yahoo.ro>
2014-11-14 11:17:27 +0200

    sconnections

--- BestChat/BestChat/App_Start/Startup.Auth.cs
+++ BestChat/BestChat/App_Start/Startup.Auth.cs
@@ -24,16 +24,16 @@ namespace BestChat
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+              //  clientId: "",
+                //clientSecret: "");
 
             //app.UseTwitterAuthentication(
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "",
+               appSecret: "");
 
             app.UseGoogleAuthentication();
 
